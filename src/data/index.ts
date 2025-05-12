42ae5171c91977d8e9674ea7169f3f4db27ad858
Živorad Milekić <zivoradmilekic@gmail.com>
2025-05-12 09:03:35 +0200

    Updated API URL

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getData(resumo_uuid: string) {
   const { data } = await axios.get(
-    `https://api.malla.rs/api/rest/resumes/${resumo_uuid}`,
+    `https://api.resumo.me/api/rest/resumes/${resumo_uuid}`,
     {
       headers: {
         "x-hasura-admin-secret": import.meta.env.HASURA_ADMIN_SECRET,
